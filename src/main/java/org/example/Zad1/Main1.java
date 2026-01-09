61bdc0d78ca535a50094c390fd5a1e2d0567dfa0
Stanislaw Sikora <remixy0@onet.pl>
2026-01-09 23:54:15 +0100

    zad1

---
+++ src/main/java/org/example/Zad1/Main1.java
@@ -0,0 +1,8 @@
+package org.example.Zad1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        if(Calculator.divide(4,2) == 2) System.out.println("Test passed");
+        else System.out.println("Test failed");
+    }
+}
